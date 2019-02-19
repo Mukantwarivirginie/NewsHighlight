c29a54fad64c4e7c72494c1ac4583ebe849c19ac
mukantwarivirginie <virgm2020@gmail.com>
2019-02-19 08:14:30 +0200

    added import in views.py

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app import app
 from .request import get_sources
 
+from .requests import get_movies,get_movie
+
 
 
 @app.route('/')
@@ -17,3 +19,20 @@ def index():
     now_showing_category = get_sources('sports')
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title, popular =popular_general, upcoming =upcoming_category, now_showing = now_showing_category )
+
+
+
+
+
+
+
+    @app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
